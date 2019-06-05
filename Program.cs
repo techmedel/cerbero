385c1694a1e410ce12aa7d9168cf971a3597dbad
Nestor Martinez Hernandez <nestor-mh@outlook.com>
2019-06-05 09:03:22 -0500

    fotmato

--- Program.cs
+++ Program.cs
@@ -16,33 +16,15 @@ namespace canserbero
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).UseKestrel()
-               .UseContentRoot(Directory.GetCurrentDirectory())
-               .ConfigureAppConfiguration((hostingContext, config) =>
-               {
-                   config
-                       .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                       .AddJsonFile("appsettings.json", true, true)
-                       .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-                       .AddJsonFile("ocelot.json")
-                       .AddEnvironmentVariables();
-               })
-               .ConfigureServices(s => {
-                   s.AddOcelot();
-               })
-               .ConfigureLogging((hostingContext, logging) =>
-               {
-                   //add your logging
-               })
-               .UseIISIntegration()
-               .Configure(app =>
-               {
-                   app.UseOcelot().Wait();
-               }).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+           WebHost.CreateDefaultBuilder(args)
+               .ConfigureAppConfiguration((host, config) =>
+               {
+                   config.AddJsonFile("ocelot.json");
+               })
+               .UseStartup<Startup>();
     }
 }
